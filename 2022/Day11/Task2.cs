2f1b350e5d1fe910cdbf6eb2eb9b933a8cfa7053
s-berner <sberner9@gmail.com>
2023-06-15 16:21:09 +0200

    WIP: Working on Day11

---
+++ 2022/Day11/Task2.cs
@@ -0,0 +1,11 @@
+namespace AdventOfCode2022.Day11;
+
+public class Task2 : IAdventOfCodeTask
+{
+    public void Solve(string[] input)
+    {
+        Console.WriteLine("Day 11, Task 2 solution:");
+
+        Console.WriteLine("Not finished yet!");
+    }
+}
